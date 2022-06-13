12ff7ecfb3b05dacb5d54913bbbfda9fc48ad0b8
ikir0806 <ikir2826@gmail.com>
2022-06-13 15:46:17 +0300

    completed lesson-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Form } from "./components/class/Form";
-import { Form as FormFunc } from "./components/func/Form";
-import { useState } from "react";
+/* import { Form } from "./components/class/Form";
+import { Form as FormFunc } from "./components/func/Form"; */
+import { Message } from "./components/message/Message";
+/* import { useState } from "react";*/
 export const App = () => {
-    const [toggle, setToggle] = useState(true);
+    /* const [toggle, setToggle] = useState(true); */
     return <>
-        <Form />
+        <Message text="Hello" />
+        {/* <Form />
         <hr />
         <button onClick={() => setToggle(!toggle)}>{toggle ? 'hide' : 'show'}</button>
-        {toggle && <FormFunc />}
+        {toggle && <FormFunc />} */}
     </>;
 }
---
+++ src/components/message/Message.js
@@ -0,0 +1,9 @@
+import style from './style.module.css';
+
+export const Message = (props) => {
+    return (
+        <>
+            <p className={style.background}>{props.text}</p>
+        </>
+    )
+}  
