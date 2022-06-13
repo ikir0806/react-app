12126006d789d63bbdfb910b14aa64ae3472ead4
ikir0806 <ikir2826@gmail.com>
2022-06-13 15:41:21 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* import { Form } from "./components/class/Form";
+import { Form as FormFunc } from "./components/func/Form"; */
+import { Message } from "./components/message/Message";
+/* import { useState } from "react";*/
+export const App = () => {
+    /* const [toggle, setToggle] = useState(true); */
+    return <>
+        <Message text="Hello" />
+        {/* <Form />
+        <hr />
+        <button onClick={() => setToggle(!toggle)}>{toggle ? 'hide' : 'show'}</button>
+        {toggle && <FormFunc />} */}
+    </>;
 }
-
-export default App;
