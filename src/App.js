dc1a255f59bc854bcf3f39ecdf79cba5eb082e9d
ikir0806 <ikir2826@gmail.com>
2022-06-17 16:27:39 +0300

    completed lesson-2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-/* import { Form } from "./components/class/Form";
-import { Form as FormFunc } from "./components/func/Form"; */
-import { Message } from "./components/message/Message";
-/* import { useState } from "react";*/
+import { Message } from "./message/Message";
+import { useState, useEffect } from "react";
+
 export const App = () => {
-    /* const [toggle, setToggle] = useState(true); */
+    const [messageList, setMessageList] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        let copy = Object.assign([], messageList);
+        copy.push({ author: 'user1', text: message })
+        setMessageList(copy);
+        setMessage('');
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if ((messageList[0]) &&
+                ((messageList[messageList.length - 1].author) !== 'bot')) {
+                let copy = Object.assign([], messageList);
+                copy.push({ author: 'bot', text: 'hello there' })
+                setMessageList(copy);
+            }
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [messageList]);
+
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    }
+
     return <>
-        <Message text="Hello" />
-        {/* <Form />
-        <hr />
-        <button onClick={() => setToggle(!toggle)}>{toggle ? 'hide' : 'show'}</button>
-        {toggle && <FormFunc />} */}
+        <Message text="Messenger" />
+        <ul>
+            {messageList.map((item, idx) => <li key={idx}>{item.author}: {item.text}</li>)}
+        </ul>
+        <div>
+            <input type="text" value={message} onChange={handleChange} />
+            <button onClick={handleClick}>send</button>
+        </div>
     </>;
 }
